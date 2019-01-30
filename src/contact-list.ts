cddb74b0f16d0b16f17560d913e536572c3c3bee
Daniel Porumb <daniel.porumb@reyno.co.uk>
2019-01-30 22:16:51 +0200

    updating contact details

--- src/contact-list.ts
+++ src/contact-list.ts
@@ -1,19 +1,29 @@
+import { ContactUpdated, ContactViewed } from './message';
+import { EventAggregator } from 'aurelia-event-aggregator';
 import { inject } from 'aurelia-dependency-injection';
 import { WebAPI } from './web-api';
 
-@inject(WebAPI)
-export class ContactList{
+@inject(WebAPI, EventAggregator)
+export class ContactList {
   contacts;
   selectedId = 0;
 
   constructor(
-    private api: WebAPI
-  ){}
-  
-  created(){
+    private api: WebAPI,
+    ea: EventAggregator
+  ) {
+    ea.subscribe(ContactViewed, msg => this.select(msg.contact));
+    ea.subscribe(ContactUpdated, msg =>{
+      let id = msg.contact.id;
+      let found = this.contacts.find(x => x.id == id);
+      Object.assign(found, msg.contact);
+    })
+  }
+
+  created() {
     this.api.getContactList().then(contacts => this.contacts = contacts);
   }
-  select(contact){
+  select(contact) {
     this.selectedId = contact.id;
     return true;
   }
--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,5 +1,7 @@
+import { PLATFORM } from 'aurelia-pal';
 import {FrameworkConfiguration} from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
   //config.globalResources([]);
+  config.globalResources(PLATFORM.moduleName('./elements/loading-indicator'));
 }
